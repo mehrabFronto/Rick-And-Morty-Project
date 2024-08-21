64448d97304f44f39804f5ea3f4cc1c085b0b6e8
mehrabFronto <mehrabd20@gmail.com>
2024-08-21 12:52:06 +0330

    refactor(pwa): remove static cache

--- public/sw.ts
+++ public/sw.ts
@@ -1,34 +1,7 @@
-const staticItems = [
-  "/",
-  "/@react-refresh",
-  "/@vite/client",
-  "/images/icons/android/android-launchericon-192-192.png",
-  "/images/icons/ios/144.png",
-  "/manifest.json",
-  "/src/App.css",
-  "/src/App.tsx",
-  "/src/SwDev.tsx",
-  "/src/components/CharacterDetail.tsx",
-  "/src/components/CharactersList.tsx",
-  "/src/components/Loader.tsx",
-  "/src/components/Modal.tsx",
-  "/src/components/Navbar.tsx",
-  "/src/hooks/useCharacters.ts",
-  "/src/hooks/useLocalStorage.ts",
-  "/src/index.css",
-  "/src/main.tsx",
-  "/node_modules/vite/dist/client/env.mjs",
-];
-
-const STATIC_CACHE = "static-v1";
 const DYNAMIC_CACHE = "dynamic-v1";
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(STATIC_CACHE).then((cache) => {
-      cache.addAll(staticItems);
-    })
-  );
+  console.log(e);
 });
 
 self.addEventListener("activate", (e) => {
